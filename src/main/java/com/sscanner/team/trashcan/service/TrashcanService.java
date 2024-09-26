beadefb9b127fa38fae3314e92c4eeffbcf2384e
7zrv <4014133@gmail.com>
2024-09-26 17:49:47 +0900

    feat: id를 통한 쓰레기통 조회 API 구현
    - 쓰레기통 조회를 위한 컨트롤러 엔드포인트 구현
    - 검색 실패에 대한 예외 처리 포함
    - 쓰레기통 등록 처리를 위한 TrashcanService 메서드 생성

--- src/main/java/com/sscanner/team/trashcan/service/TrashcanService.java
+++ src/main/java/com/sscanner/team/trashcan/service/TrashcanService.java
@@ -1,6 +1,7 @@
 package com.sscanner.team.trashcan.service;
 
 import com.sscanner.team.global.exception.BadRequestException;
+import com.sscanner.team.global.exception.ExceptionCode;
 import com.sscanner.team.trashcan.entity.Trashcan;
 import com.sscanner.team.trashcan.repository.TrashcanRepository;
 import com.sscanner.team.trashcan.requestDto.RegisterTrashcanRequestDto;
@@ -9,6 +10,8 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+
+import static com.sscanner.team.global.exception.ExceptionCode.NOT_FOUND_TRASHCAN_ID;
 
 @RequiredArgsConstructor
 @Service
@@ -30,7 +33,8 @@ public class TrashcanService {
     public TrashcanResponseDto getTrashcanById(Long trashcanId) {
 
         Trashcan trashcan = trashcanRepository.findById(trashcanId)
-                .orElseThrow(() -> new BadRequestException(NOT_FOUND_ORDER_ID));
+                .orElseThrow(() -> new BadRequestException(NOT_FOUND_TRASHCAN_ID));
 
+        return TrashcanResponseDto.from(trashcan);
     }
 }
