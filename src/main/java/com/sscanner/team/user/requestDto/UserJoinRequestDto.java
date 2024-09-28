1df49c447756fb701752cc42d28a730d0bce8071
김지은 <wldms5119@naver.com>
2024-09-28 13:31:59 +0900

    refactor: 이메일 유효성 검사 메시지 수정

--- src/main/java/com/sscanner/team/user/requestDto/UserJoinRequestDto.java
+++ src/main/java/com/sscanner/team/user/requestDto/UserJoinRequestDto.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 
 public record UserJoinRequestDto(
 
-        @NotBlank(message = "에미일이 비어있습니다.")
+        @NotBlank(message = "이메일이 비어있습니다.")
         String email,
 
         @NotBlank(message = "비밀번호가 비어있습니다.")
