c476e6ecc48ba9a2e23979b3c942571a11cc26e4
김지은 <wldms5119@naver.com>
2024-10-03 05:12:42 +0900

    feat: 리프레시 토큰 관리 기능 구현
    - 리프레시 토큰 테이블 엔티티 및 레포지토리 구성
    
    - 토큰 재발급 서비스 로직 추가: 기존 리프레시 토큰 삭제 후 새로운 토큰 발급 및 쿠키 저장
    
    - 만료된 리프레시 토큰 처리 및 예외 처리 로직 추가

---
+++ src/main/java/com/sscanner/team/user/service/ReissueService.java
@@ -0,0 +1,91 @@
+package com.sscanner.team.user.service;
+
+import com.sscanner.team.global.common.response.ApiResponse;
+import com.sscanner.team.global.exception.BadRequestException;
+import com.sscanner.team.global.exception.ExceptionCode;
+import com.sscanner.team.jwt.JWTUtil;
+import com.sscanner.team.user.entity.Refresh;
+import com.sscanner.team.user.repository.RefreshRepository;
+import com.sscanner.team.user.responseDto.RefreshResponseDto;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+@RequiredArgsConstructor
+public class ReissueService {
+    private final JWTUtil jwtUtil;
+    private final RefreshRepository refreshRepository;
+
+    public ApiResponse<RefreshResponseDto> reissueToken(HttpServletRequest request, HttpServletResponse response) {
+        String refresh = null;
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("refresh")) {
+                refresh = cookie.getValue();
+            }
+        }
+
+        // 리프레시 토큰 존재 체크
+        if (refresh == null) {
+            throw new BadRequestException(ExceptionCode.NOT_EXIST_REFRESH_TOKEN);
+        }
+
+        // 리프레시토큰 만료 체크
+        try {
+            jwtUtil.isExpired(refresh);
+        } catch (ExpiredJwtException e) {
+            throw new BadRequestException(ExceptionCode.EXPIRED_REFRESH_TOKEN);
+        }
+
+        // 리프레시 토큰이 맞는지 체크
+        String category = jwtUtil.getCategory(refresh);
+        if (!category.equals("refresh")) {
+            throw new BadRequestException(ExceptionCode.INVALID_REFRESH_TOKEN);
+        }
+
+        // DB에 해당 리프레시 토큰 존재 체크
+        Boolean isExist = refreshRepository.existsByRefresh(refresh);
+        if (!isExist) {
+            throw new BadRequestException(ExceptionCode.INVALID_REFRESH_TOKEN);
+        }
+
+        String email = jwtUtil.getEmail(refresh);
+        String authority = jwtUtil.getAuthority(refresh);
+
+        // 새 access 토큰 발급
+        String newAccess = jwtUtil.createJwt("access", email, authority, 600000L);
+        String newRefresh = jwtUtil.createJwt("refresh", email, authority, 86400000L);
+
+        // 기존 Refresh 토큰 삭제 후 새 Refresh 토큰 저장
+        refreshRepository.deleteByRefresh(refresh);
+        addRefresh(email, newRefresh, 86400000L);
+
+        response.setHeader("access", newAccess);
+        response.addCookie(createCookie("refresh", newRefresh));
+
+        RefreshResponseDto responseDto = RefreshResponseDto.from(newAccess, newRefresh);
+        return ApiResponse.ok(200, responseDto, "토큰 재발급 성공");
+    }
+
+    // 리프레시 토큰 추가 후 저장
+    private void addRefresh(String email, String newRefresh, Long expiredMs) {
+        Date date = new Date(System.currentTimeMillis() + expiredMs);
+        Refresh refreshEntity = new Refresh(email, newRefresh, date.toString());
+        refreshRepository.save(refreshEntity);
+    }
+
+    private Cookie createCookie(String key, String value) {
+        Cookie cookie = new Cookie(key, value);
+        cookie.setMaxAge(24 * 60 * 60); // 1일 설정
+        cookie.setHttpOnly(true); // js로 해당 쿠키 접근 불가하게 설정
+        //cookie.setSecure(true);  // https 통신 진행 시 사용
+        //cookie.setPath("/");  // 쿠키가 적용될 범위
+        return cookie;
+    }
+}
