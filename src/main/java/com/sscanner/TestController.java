1736d73b3b3d940a89315f251ec74c947e5afef0
jhk01007 <jhk001007@naver.com>
2024-10-02 17:44:17 +0900

    test: 배포 테스트

--- src/main/java/com/sscanner/TestController.java
+++ src/main/java/com/sscanner/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/")
     public String hello() {
-        return "hello";
+        return "hello world";
     }
 
     @GetMapping("/health")
