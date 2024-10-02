fa502e5a0eb890f82faf06553d51de047f272178
ryu <jeungwon28@gmail.com>
2024-10-02 10:57:34 +0900

    feat: PointUpdateRequestDto 추가

---
+++ src/main/java/com/sscanner/team/points/requestdto/PointUpdateRequestDto.java
@@ -0,0 +1,23 @@
+package com.sscanner.team.points.requestdto;
+
+import com.sscanner.team.User;
+import com.sscanner.team.UserPoint;
+
+
+public record PointUpdateRequestDto(
+        String userPointId,
+        User user,
+        Integer newPoint
+) {
+    public static PointUpdateRequestDto of(String userPointId, User user, Integer newPoint) {
+        return new PointUpdateRequestDto(userPointId, user, newPoint);
+    }
+
+    public UserPoint toEntity() {
+        return UserPoint.builder()
+                .userPointId(userPointId)
+                .user(user)
+                .point(newPoint)
+                .build();
+    }
+}
