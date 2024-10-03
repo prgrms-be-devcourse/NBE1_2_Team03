49d16e5b826f6fc9d0d2fdd8f67707423910f3c5
김지은 <wldms5119@naver.com>
2024-10-03 15:52:49 +0900

    불필요한 로그 파일 삭제 및 저장소 정리

--- src/main/java/com/sscanner/team/global/exception/ExceptionCode.java
+++ src/main/java/com/sscanner/team/global/exception/ExceptionCode.java
@@ -53,9 +53,7 @@ public enum ExceptionCode {
     EXPIRED_REFRESH_TOKEN(400, "리프레시 토큰이 만료되었습니다."),
     INVALID_REFRESH_TOKEN(400, "유효하지 않은 리프레시 토큰입니다."),
     EXPIRED_ACCESS_TOKEN(401, "엑세스 토큰이 만료되었습니다."),
-    INVALID_ACCESS_TOKEN(401, "유효하지 않은 액세스 토큰입니다.");
-    ;
-    USER_NOT_FOUND(400,"해당 유저가 존재하지 않습니다."),
+    INVALID_ACCESS_TOKEN(401, "유효하지 않은 액세스 토큰입니다."),
     NOT_EXIST_TRASHCAN_IMG_ID(400, "해당하는 쓰레기통 이미지가 존재하지 않습니다.");
 
     private final int code;
