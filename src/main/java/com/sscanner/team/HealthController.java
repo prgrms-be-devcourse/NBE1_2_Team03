7b8153aa4bcce9bd89d2f53afabb2b69b40d8699
jhk01007 <jhk001007@naver.com>
2024-10-03 12:09:01 +0900

    test: 무중단 배포가 잘되는지 테스트
    - Nginx 쪽 fail_timeout을 30s -> 10s
    - 배포 이후 새로고침을 해도 서버가 끊기지 않는지
    - 수정된 부분이 반영이 잘되는지

--- src/main/java/com/sscanner/team/HealthController.java
+++ src/main/java/com/sscanner/team/HealthController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
     @GetMapping("/")
     public String hello() {
-        return "hello world5";
+        return "hello world";
     }
 
     @GetMapping("/health")
