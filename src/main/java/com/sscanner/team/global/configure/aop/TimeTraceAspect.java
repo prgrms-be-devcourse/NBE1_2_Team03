41383ecc8cd0f3e633ef5cf7832db636d443420a
jhk01007 <jhk001007@naver.com>
2024-10-08 14:38:19 +0900

    refactor: TimeTraceAspect 패키지 변경

--- src/main/java/com/sscanner/team/global/configure/aop/TimeTraceAspect.java
+++ src/main/java/com/sscanner/team/global/configure/aop/TimeTraceAspect.java
@@ -1,5 +1,6 @@
 package com.sscanner.team.global.configure.aop;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -7,15 +8,16 @@ import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Slf4j
 public class TimeTraceAspect {
 
-    @Around("@annotation(org.example.gc_coffee.global.configure.aop.TimeTrace)")
+    @Around("@annotation(com.sscanner.team.global.configure.aop.TimeTrace)")
     public Object measureExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
         Object proceed = joinPoint.proceed();
         long executionTime = System.currentTimeMillis() - start;
 
-        System.out.println(joinPoint.getSignature() + " 실행시간은~~~~~~~ " + executionTime + "ms");
+        log.info("{} 실행시간은~~~~~~~ {}ms", joinPoint.getSignature(), executionTime);
         return proceed;
     }
 }
