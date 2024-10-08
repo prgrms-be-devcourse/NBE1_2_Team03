51525f619538f073a1abe98181f806579c064bb5
jhk01007 <jhk001007@naver.com>
2024-10-08 14:33:48 +0900

    refactor: HealthController global/utils 패키지로 이동

--- src/main/java/com/sscanner/team/global/utils/HealthController.java
+++ src/main/java/com/sscanner/team/global/utils/HealthController.java
@@ -1,4 +1,4 @@
-package com.sscanner.team;
+package com.sscanner.team.global.utils;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
