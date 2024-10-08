d8c030385879e68dde6f7063b08edaabefb4f26e
김지은 <wldms5119@naver.com>
2024-10-08 11:08:53 +0900

    feat: Userutil 추가
    - 현재 로그인한 유저 정보 가져오는 유틸 추가

---
+++ src/main/java/com/sscanner/team/global/utils/UserUtils.java
@@ -0,0 +1,24 @@
+package com.sscanner.team.global.utils;
+
+import com.sscanner.team.User;
+import com.sscanner.team.global.exception.DuplicateException;
+import com.sscanner.team.global.exception.ExceptionCode;
+import com.sscanner.team.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserUtils {
+    private final UserRepository userRepository;
+
+    // 현재 로그인한 유저 정보 가져옴
+    public User getUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new DuplicateException(ExceptionCode.USER_NOT_FOUND));
+    }
+}
