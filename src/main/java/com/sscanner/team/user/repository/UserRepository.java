0c72783730f56163d90468a8df5ce87589eccd3e
김지은 <wldms5119@naver.com>
2024-10-12 17:14:56 +0900

    fix: 회원정보 삭제 스케줄러 오류수정

--- src/main/java/com/sscanner/team/user/repository/UserRepository.java
+++ src/main/java/com/sscanner/team/user/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.sscanner.team.user.repository;
 
 import com.sscanner.team.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
@@ -12,6 +14,10 @@ public interface UserRepository extends JpaRepository<User, String> {
     boolean existsByNickname(String nickname);
     boolean existsByPhone(String phone);
     Optional<User> findByEmail(String email);
-    List<User> findAllByDeletedAt(LocalDateTime dateTime); // 회원 탈퇴 30일 지난 사용자
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM user WHERE deleted_at < :date", nativeQuery = true)
+    void deleteAllByDeletedAtBefore(LocalDateTime date);// 회원 탈퇴 30일 지난 사용자
 }
 
--- src/main/java/com/sscanner/team/user/scheduler/UserDeletionScheduler.java
+++ src/main/java/com/sscanner/team/user/scheduler/UserDeletionScheduler.java
@@ -1,13 +1,11 @@
 package com.sscanner.team.user.scheduler;
 
-import com.sscanner.team.User;
 import com.sscanner.team.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -19,10 +17,9 @@ public class UserDeletionScheduler {
     public void deleteOldUsers() {
 
         LocalDateTime date = LocalDateTime.now().minusDays(30);
-        List<User> usersToDelete = userRepository.findAllByDeletedAt(date);
 
         // 기간 지난 사용자 물리적 삭제
-        userRepository.deleteAll(usersToDelete);
+        userRepository.deleteAllByDeletedAtBefore(date);
 
     }
 }
