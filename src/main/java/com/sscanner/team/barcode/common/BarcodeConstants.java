fd4a8104b6dd7fa764d4745bf143d4de387f5f16
ryu <jeungwon28@gmail.com>
2024-10-21 22:42:28 +0900

    refactor: BarcodeConstants 생성

---
+++ src/main/java/com/sscanner/team/barcode/common/BarcodeConstants.java
@@ -0,0 +1,8 @@
+package com.sscanner.team.barcode.common;
+
+public class BarcodeConstants {
+    public static final String BARCODE_TEXT_TEMPLATE = "ProductID: %d, UserID: %s";
+    public static final int BARCODE_WIDTH = 300;
+    public static final int BARCODE_HEIGHT = 100;
+    public static final String IMAGE_FORMAT = "png";
+}
