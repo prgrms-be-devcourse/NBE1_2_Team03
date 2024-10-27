008844296909ed61acd4c60fedf29b302c9cb796
ryu <jeungwon28@gmail.com>
2024-10-27 19:02:27 +0900

    fix: SonarCloud 반영

--- src/main/java/com/sscanner/team/global/configure/SecurityConfig.java
+++ src/main/java/com/sscanner/team/global/configure/SecurityConfig.java
@@ -48,9 +48,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf((csrf) -> csrf
-                .ignoringRequestMatchers("/api/**")  // 특정 경로에서만 CSRF 비활성화
-        );
+        http.csrf((csrf) -> csrf.disable());  // 특정 경로에서만 CSRF 비활성화
         http.formLogin(AbstractHttpConfigurer::disable);
         http.httpBasic(AbstractHttpConfigurer::disable);
 
